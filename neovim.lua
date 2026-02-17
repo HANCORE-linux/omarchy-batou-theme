fa2fe82c889a6908109f2427389fa3a1c08c62d8
HANCORE <m.fattah-ahmad@gmx.de>
2026-02-17 19:16:09 +0100

    Change base0D color for functions and keywords

--- neovim.lua
+++ neovim.lua
@@ -22,7 +22,7 @@ return {
 				base0A = "#c6c4b0", -- Classes, types, yellow
 				base0B = "#a5a297", -- Strings, green
 				base0C = "#e1d574", -- Support, regex, cyan
-				base0D = "#777472", -- Functions, keywords, blue
+				base0D = "#605d5b", -- Functions, keywords, blue
 				base0E = "#8a8575", -- Keywords, storage, magenta
 				base0F = "#8f4445", -- Deprecated, brown/yellow
 			},
